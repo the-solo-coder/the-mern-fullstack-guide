32d2264e45eff29de42e3d0b7a48875f7c7e5e16
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-27 15:55:28 -0400

    Create sample as of end of lesson 145

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -52,17 +52,40 @@ const Auth = () => {
             isValid: false,
           },
         },
-         false
+        false
       );
     }
 
     setIsLoginMode((prevMode) => !prevMode);
   };
 
-  const authSubmitHandler = (event) => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    // send this later to the backend!
-    console.log(formState.inputs);
+
+    if (isLoginMode) {
+    } else {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+
+        console.log(responseData);
+      } catch (err) {
+        //do error handling later
+        console.log(err);
+      }
+    }
+
     auth.login();
   };
 
