e4205b853ecb9f881a33780b8bf778ae8c59c6e4
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-29 20:18:17 -0400

    Create sample as of end of lesson 150

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,51 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortController = new AbortController();
+      activeHttpRequests.current.push(httpAbortController);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          headers,
+          body,
+          signal: httpAbortController.signal
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        return responseData;
+      } catch (err) {
+        setError(err.message || "Something went wrong, please try again");
+      }
+
+      setIsLoading(false);
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+      //clean up function
+      return () => {
+          activeHttpRequests.current.forEach(abortController => abortController.abort())
+      }
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
