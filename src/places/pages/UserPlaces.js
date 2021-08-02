33a964626e20795b55fda056b1cdd91d832d6f5a
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-08-02 17:14:02 -0400

    Create sample as of end of lesson 154

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import PlaceList from '../components/PlaceList';
+import PlaceList from "../components/PlaceList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous buildings in the globe!',
-        imageUrl: 'https://www.great-towers.com/sites/default/files/2019-07/tower_0.jpg',
-        address: '20 W 34th St, New York, NY 10001, USA',
-        location: {
-            lat: 40.74861116670947,
-            lng: -73.9856751309138
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Emp. State Building',
-        description: 'One of the most famous buildings in the globe!',
-        imageUrl: 'https://www.great-towers.com/sites/default/files/2019-07/tower_0.jpg',
-        address: '20 W 34th St, New York, NY 10001, USA',
-        location: {
-            lat: 40.74861116670947,
-            lng: -73.9856751309138
-        },
-        creator: 'u2'
-    }
-];
+const UserPlaces = (props) => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const userId = useParams().userId;
 
-const UserPlaces = props => {
-    const userId = useParams().userId;
+  useEffect(() => {
+    const fetchPlaces = async () => {
+    try {
+      const responseData = await sendRequest(
+        `http://localhost:5000/api/places/user/${userId}`
+      );
 
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+      setLoadedPlaces(responseData.places);
+    } catch (err) {}
+  };
 
-    return <PlaceList items={loadedPlaces}/>
+  fetchPlaces();
+}, [sendRequest, userId]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
+  );
 };
 
 export default UserPlaces;
