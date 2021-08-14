f191849c3a1ffdee9df59a0f92a5d56047409d2a
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-08-14 13:11:40 -0400

    Create sample as of end of lesson 185

--- src/App.js
+++ src/App.js
@@ -20,12 +20,18 @@ const App = () => {
 
   //useCallback is used to avoid infinite loops
   //with [] so there are no dependencies and once created it will never be recreated
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
     localStorage.setItem(
       "userData",
-      JSON.stringify({ userId: uid, token: token })
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
     );
   }, []);
 
@@ -38,8 +44,17 @@ const App = () => {
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
 
-    if (storedData && storedData.token && storedData.userId) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      storedData.userId &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
